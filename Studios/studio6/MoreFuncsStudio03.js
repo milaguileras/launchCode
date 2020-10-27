2cc7dac68321cfcf9168025771084b6c8b9908ba
milaguileras <milaguileras@gmail.com>
2020-10-27 13:22:37 -0500

    javascript

---
+++ Studios/studio6/MoreFuncsStudio03.js
@@ -0,0 +1,18 @@
+//Sample arrays for testing:
+let nums1 = [5, 10, 2, 42];
+let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
+let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
+//Sort each array in ascending order.
+
+nums1.sort(function(a, b){return a-b})
+nums2.sort(function(a, b){return a-b})
+nums3.sort(function(a, b){return a-b})
+
+
+
+//Sort each array in decending order.
+
+nums1.reverse(function(b, a){return b-a})
+nums2.reverse(function(a, b){return a-b})
+nums3.reverse(function(a, b){return a-b})
