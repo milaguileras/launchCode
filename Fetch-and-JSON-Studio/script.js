e58677f6474b5556f443bd221c1dd0fd64fb7760
milaguileras <milaguileras@gmail.com>
2020-12-08 11:46:09 -0300

    studio class 15

---
+++ Fetch-and-JSON-Studio/script.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", function () {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (json) {
+            const div = document.getElementById("container")
+            for (let i = 0; i < json.length; i++) {
+                div.innerHTML += `
+                 <div class = "astronaut">
+                 <div class = <"bio">
+                 <h3>${json[i].firstName}${json[i].lastname}</h3>
+            <ul>
+                <li>${json[i].hoursInSpace}</li>
+                <li>${json[i].active}</li>
+                <li>${json[i].skills}</li>
+            </ul>
+            </div>
+            <img class = "avatar" src="${json[i].picture}">`
+
+            }
+
+        })
+    })
+})
