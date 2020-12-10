e096060401df3c25718a352030079b4721d7a5d7
milaguileras <milaguileras@gmail.com>
2020-12-10 16:18:55 -0300

    Updating

--- Fetch-and-JSON-Studio/script.js
+++ Fetch-and-JSON-Studio/script.js
@@ -1,7 +1,9 @@
 window.addEventListener("load", function () {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
         response.json().then(function (json) {
+            console.log(json)
             const div = document.getElementById("container")
+            console.log(div)
             for (let i = 0; i < json.length; i++) {
                 div.innerHTML += `
                  <div class = "astronaut">
